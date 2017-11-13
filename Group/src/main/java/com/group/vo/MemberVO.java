e6f7208d28affd491c92c8725e231d94cb8b5c77
IT58 <IT58@IT58-HP>
2017-11-13 10:53:45 +0900

    회원가입

--- Group/src/main/java/com/group/vo/MemberVO.java
+++ Group/src/main/java/com/group/vo/MemberVO.java
@@ -6,5 +6,153 @@ import org.apache.ibatis.type.Alias;
 public class MemberVO {
 	//dto와 같은 역할
 	//getter / setter 추가
+	private int membernum;
+	private String rankname;
+	private String departname;
+	private String id;
+	private String password;
+	private String name;
+	private String email;
+	private int phone;
+	private String birth;
+	private String address1;
+	private String address2;
+	private String join;
+	private String photo;
+	private String sign;
+	private int memstatus;
+	private int admin;
+	
+	public MemberVO() {}
+
+	public int getMembernum() {
+		return membernum;
+	}
+
+	public void setMembernum(int membernum) {
+		this.membernum = membernum;
+	}
+
+	public String getRankname() {
+		return rankname;
+	}
+
+	public void setRankname(String rankname) {
+		this.rankname = rankname;
+	}
+
+	public String getDepartname() {
+		return departname;
+	}
+
+	public void setDepartname(String departname) {
+		this.departname = departname;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getPhone() {
+		return phone;
+	}
+
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+
+	public String getBirth() {
+		return birth;
+	}
+
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+
+	public String getAddress1() {
+		return address1;
+	}
+
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+
+	public String getAddress2() {
+		return address2;
+	}
+
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+
+	public String getJoin() {
+		return join;
+	}
+
+	public void setJoin(String join) {
+		this.join = join;
+	}
+
+	public String getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+
+	public String getSign() {
+		return sign;
+	}
+
+	public void setSign(String sign) {
+		this.sign = sign;
+	}
+
+	public int getMemstatus() {
+		return memstatus;
+	}
+
+	public void setMemstatus(int memstatus) {
+		this.memstatus = memstatus;
+	}
+
+	public int getAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(int admin) {
+		this.admin = admin;
+	}
+
+	
 	
 }
