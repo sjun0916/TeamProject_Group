724b3c59d88fae7b3e9737f28068213afedb75ec
IT66 <IT66@IT66-HP>
2017-11-13 13:53:57 +0900

    test

---
+++ Group/src/main/java/com/group/vo/CalendarVO.java
@@ -0,0 +1,58 @@
+package com.group.vo;
+
+public class CalendarVO {
+	private int calNum;	//seqence로 설정됨
+	private int memberNum;
+	private String startDate;
+	private String endDate;
+	private String title;
+	private String content;
+	private int kind;	//1-회사 2-부서 3-개인
+
+	//생성자
+	public CalendarVO() {}
+	//te
+	//getter() /setter()
+	public int getCalNum() {
+		return calNum;
+	}
+	public void setCalNum(int calNum) {
+		this.calNum = calNum;
+	}
+	public int getMemberNum() {
+		return memberNum;
+	}
+	public void setMemberNum(int memberNum) {
+		this.memberNum = memberNum;
+	}
+	public String getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+	public String getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getKind() {
+		return kind;
+	}
+	public void setKind(int kind) {
+		this.kind = kind;
+	}
+}
